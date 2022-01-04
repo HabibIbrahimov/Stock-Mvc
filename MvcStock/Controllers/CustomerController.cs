8a444d3f7ee662e280f2f23bf49a14c06544f6c9
HabibIbrahimov <habibyi@code.edu.az>
2022-01-04 18:07:16 +0400

    feat:listed customers

---
+++ MvcStock/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStock.Models.Entity;
+
+namespace MvcStock.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        MvcDbStockEntities db = new MvcDbStockEntities();
+        public ActionResult Index()
+        {
+            var value = db.TBLCUSTOMERS.ToList();
+            return View(value);
+        }
+    }
+}
