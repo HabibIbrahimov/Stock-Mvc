15bcd3aabad62bff156fa1b6a73919f719e5feb6
HabibIbrahimov <habibyi@code.edu.az>
2022-01-04 16:30:11 +0400

    feat:listed categories

--- MvcStock/Controllers/CategoryController.cs
+++ MvcStock/Controllers/CategoryController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcStock.Models.Entity;
 
 namespace MvcStock.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+        MvcDbStockEntities db = new MvcDbStockEntities();
         public ActionResult Index()
         {
-            return View();
+            var value = db.TBLCATEGORIES.ToList();
+            return View(value);
         }
     }
 }
