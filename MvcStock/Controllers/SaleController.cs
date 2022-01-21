e623a15b68038497264585e1dc1c19ea7dc4521c
HabibIbrahimov <habibyi@code.edu.az>
2022-01-21 23:25:18 +0400

    feat:added popup modal and created sales page

---
+++ MvcStock/Controllers/SaleController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStock.Models.Entity;
+
+namespace MvcStock.Controllers
+{
+    public class SaleController : Controller
+    {
+        // GET: Sale
+        MvcDbStockEntities db = new MvcDbStockEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult NewSale()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewSale(TBLSALES p)
+        {
+            db.TBLSALES.Add(p);
+            db.SaveChanges();
+            return View("Index");
+        }
+    }
+}
